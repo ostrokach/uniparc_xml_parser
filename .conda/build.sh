f79b9b4f9acf14811af2626fd338ae9f9ab87839
ostrokach <ostrokach@gmail.com>
2018-02-22 14:53:13 -0500

    Try new syntax for any target

--- .conda/build.sh
+++ .conda/build.sh
@@ -5,11 +5,10 @@ set -u
 # Set CARGO linker and archiver
 mkdir .cargo
 cat <<EOF > .cargo/config
-[target]
+[target.{}]
 linker = "${CC}"
 ar = "${AR}"
 EOF
-cat .cargo/config
 # Alternatively, we could have used environment variables
 # export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
 # export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_ARCHIVER="${AR}"
